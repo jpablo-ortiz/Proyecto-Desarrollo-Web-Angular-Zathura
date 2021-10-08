6d0007689aaeecc1046c4a3d1d7e316236465881
Juan Pablo <ortizrubio09@gmail.com>
2021-10-07 23:30:02 -0500

    Inicio entendimiento html con angular e implementación del routing.
    Co-authored-by: Kenneth David Leonel Triana <kennethLeonel@users.noreply.github.com>

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { MenuComponent } from './menu/menu/menu.component';
+
+const routes: Routes = [
+  { path: 'menu', component: MenuComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'menu' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { MenuComponent } from './menu/menu/menu.component';
-import { InfoTripulanteComponent } from './menu/info-tripulante/info-tripulante.component';
-import { InfoTripulacionComponent } from './menu/info-tripulacion/info-tripulacion.component';
-import { InfoNaveComponent } from './menu/info-nave/info-nave.component';
-import { InfoEstrellaComponent } from './estrellas/info-estrella/info-estrella.component';
-import { NavegacionEstrellasComponent } from './estrellas/navegacion-estrellas/navegacion-estrellas.component';
-import { NavegacionPlanetasComponent } from './planetas/navegacion-planetas/navegacion-planetas.component';
-import { VistaPlanetaComponent } from './planetas/vista-planeta/vista-planeta.component';
-import { ListaProductosCompraComponent } from './planetas/lista-productos-compra/lista-productos-compra.component';
-import { ListaProductosVentaComponent } from './planetas/lista-productos-venta/lista-productos-venta.component';
 import { TransaccionCompraComponent } from './comercio/transaccion-compra/transaccion-compra.component';
 import { TransaccionVentaComponent } from './comercio/transaccion-venta/transaccion-venta.component';
+import { InfoEstrellaComponent } from './estrellas/info-estrella/info-estrella.component';
+import { NavegacionEstrellasComponent } from './estrellas/navegacion-estrellas/navegacion-estrellas.component';
+import { InfoNaveComponent } from './menu/info-nave/info-nave.component';
+import { InfoTripulacionComponent } from './menu/info-tripulacion/info-tripulacion.component';
+import { InfoTripulanteComponent } from './menu/info-tripulante/info-tripulante.component';
+import { MenuComponent } from './menu/menu/menu.component';
+import { ListaProductosCompraComponent } from './planetas/lista-productos-compra/lista-productos-compra.component';
+import { ListaProductosVentaComponent } from './planetas/lista-productos-venta/lista-productos-venta.component';
+import { NavegacionPlanetasComponent } from './planetas/navegacion-planetas/navegacion-planetas.component';
+import { VistaPlanetaComponent } from './planetas/vista-planeta/vista-planeta.component';
+
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { TransaccionVentaComponent } from './comercio/transaccion-venta/transacc
     TransaccionVentaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
